8389f467525f0db5a9f5d3ef93a5718e90695622
IcyLeave6109 <JulioHenrique31501234@gmail.com>
2024-07-29 21:07:33 -0300

    Refactor scripts

--- ui/screens/Address.gd
+++ ui/screens/Address.gd
@@ -3,12 +3,7 @@ extends Label
 var monero_script = load("res://monero_wrapper.cs")
 var monero = monero_script.new()
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	%Address.text = monero.getAddress(0, 0)
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
